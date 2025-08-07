9a48054ca9bf736614c3dc5d55941eb64b8e9ccc
mhelmih <helmihibatullah52@gmail.com>
2025-08-07 15:24:41 +0700

    refactor: Remove ReadingProgress and related functionality from BookDetail, simplifying the book detail view and updating book information handling

--- sample-project/src/gui/book_detail.py
+++ sample-project/src/gui/book_detail.py
@@ -9,8 +9,6 @@ sys.path.append('./src')
 
 from src.book.book import Book
 from src.book.book_collection import BookCollection
-from src.reading_progress.reading_progress import ReadingProgress
-from src.reading_progress.reading_progress_collection import ReadingProgressCollection
 from src.note.note_collection import NoteCollection
 from src.note.export_note import export_to_markdown
 
@@ -23,10 +21,8 @@ class BookDetail:
 
     def __init__(self):
         self.book_collection = BookCollection()
-        self.reading_progress_collection = ReadingProgressCollection()
         self.note_collection = NoteCollection()
         self.book_collection.set_db("read_buddy.db")
-        self.reading_progress_collection.set_db("read_buddy.db")
         self.note_collection.set_db("read_buddy.db")
         self.file_picker = ft.FilePicker(on_result=self.save_result)
         self.has_upload_cover = False
@@ -66,7 +62,6 @@ class BookDetail:
         self.page = page
         self.page.controls.clear() 
         self.book_id = int(params.get("id_buku"))
-        reading_progress = self.reading_progress_collection.get_reading_progress(self.book_id)
         book = self.book_collection.get_by_id(self.book_id)
 
         page_title = ft.Text(value="DETAIL BUKU " + book.get_bookTitle(), overflow=ft.TextOverflow.ELLIPSIS, weight=ft.FontWeight.BOLD, width=500)
@@ -90,42 +85,17 @@ class BookDetail:
             hint_text=to_capitalized_first_word(book.get_bookStatus())
         )
         book_status_field.value = book.get_bookStatus()
-        initial_book_status = book.get_bookStatus()
-        current_page_field = ft.TextField(value=reading_progress.getCurrentPage(),
-                                        input_filter=ft.InputFilter(allow=True, regex_string=r"[0-9]", replacement_string=""),
-                                        read_only=True)
-        detail_content = (
-            f"Kamu mulai membaca buku pada tanggal {reading_progress.getStartDate()}\n"
-            f"Sudah {reading_progress.getDayCount()} hari sejak kamu memulai pembacaan\n"
-            f"Sekarang adalah pembacaan yang ke-{reading_progress.getReadingSession()}"
-        )
-        
-        detail_field = ft.TextField(value=detail_content, read_only=True, multiline=True)
 
 
         def update_data(e):
-            if (int(current_page_field.value) > int(total_pages_field.value)):
-                snack_bar = ft.SnackBar(
-                    content=ft.Text("Halaman sekarang tidak boleh melebihi halaman total!"),
-                )
-                self.page.snack_bar = snack_bar
-                snack_bar.open = True
-                self.page.update()
-            else :
-                if ((initial_book_status == "sudah dibaca" and book_status_field.value == "Sedang Dibaca") or (initial_book_status == "ingin dibaca" and book_status_field.value == "Sedang Dibaca")):
-                    temp_reading_session = reading_progress.getReadingSession()
-                    reading_progress.setReadingSession(temp_reading_session + 1)
-                self.reading_progress_collection.update_reading_progress(ReadingProgress(self.book_id, reading_progress.getReadingSession(), int(current_page_field.value), reading_progress.getStartDate()))
-                self.book_collection.update_book(Book(self.book_id, book_title_field.value, book_status_field.value.lower(), int(total_pages_field.value), book.get_isFavorite()))
-                self.page.go("/")
+            self.book_collection.update_book(Book(self.book_id, book_title_field.value, book_status_field.value.lower(), int(total_pages_field.value), book.get_isFavorite(), book.get_coverImage()))
+            self.page.go("/")
 
         def delete_book(e):
             self.book_collection.delete_by_id(self.book_id)
-            self.reading_progress_collection.delete_by_id(self.book_id)
             self.page.go("/")
 
         view_notes_button = ft.ElevatedButton(text="Lihat Catatan", width=150, on_click= lambda _: self.page.go("/DisplayCatatan/" + str(self.book_id)))
-        record_progress_button = ft.ElevatedButton(text="Catat Progres Pembacaan", on_click= lambda _: self.page.go("/CatatProgresPembacaan/" + str(self.book_id)))
         update_button = ft.ElevatedButton(text="Update", width=150, on_click=update_data)
         delete_book_button = ft.ElevatedButton(text="Hapus Buku", on_click=delete_book)
         
@@ -206,10 +176,6 @@ class BookDetail:
                     total_pages_field,
                     ft.Text(value="Status Buku", weight=30),
                     book_status_field,
-                    ft.Text(value="Halaman Sekarang", weight=30),
-                    current_page_field,
-                    ft.Text(value="Detail", weight=30),
-                    detail_field,
                 ],
                 alignment=ft.MainAxisAlignment.SPACE_AROUND
             ),
@@ -261,7 +227,6 @@ class BookDetail:
                 [
                     view_notes_button,
                     export_notes_button,
-                    record_progress_button,
                     update_button,
                     favorite_button
                 ],
